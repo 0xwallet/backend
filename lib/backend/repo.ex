a89ca43d1da62afde1f378ad532609c8a3117134
ljzn <forbitcoincash@gmail.com>
2019-05-17 18:44:07 +0800

    Use Env In Dev Mode

--- lib/backend/repo.ex
+++ lib/backend/repo.ex
@@ -4,17 +4,13 @@ defmodule Backend.Repo do
     adapter: Ecto.Adapters.Postgres
 
   def init(_, config) do
-    if System.get_env("MIX_ENV") == "prod" do
-      config =
-        config
-        |> Keyword.put(:username, System.get_env("PGUSER"))
-        |> Keyword.put(:password, System.get_env("PGPASSWORD"))
-        |> Keyword.put(:database, System.get_env("PGDATABASE"))
-        |> Keyword.put(:hostname, System.get_env("PGHOST"))
-        |> Keyword.put(:port, System.get_env("PGPORT") |> String.to_integer)
-      {:ok, config}
-    else
-      {:ok, config}
-    end
+    config =
+      config
+      |> Keyword.put(:username, System.get_env("PGUSER"))
+      |> Keyword.put(:password, System.get_env("PGPASSWORD"))
+      |> Keyword.put(:database, System.get_env("PGDATABASE"))
+      |> Keyword.put(:hostname, System.get_env("PGHOST"))
+      |> Keyword.put(:port, System.get_env("PGPORT") |> String.to_integer)
+    {:ok, config}
   end
 end
